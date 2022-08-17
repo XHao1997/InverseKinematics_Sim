67f61ddec9c4c33a40e8a325894b77735624a2df
HaoXu <2685146425@qq.com>
2022-08-17 23:01:40 +0800

    add FKin plot Function

--- Sim.py
+++ Sim.py
@@ -4,20 +4,23 @@ import matplotlib.pyplot as plt
 import pygame
 
 '''ROBOT MODEL'''
+
+
 class robot_arm_2dof:
-    def __init__(self, l, m, ag):
+    def __init__(self, l, m):
         self.l = l  # link length
         self.m = m  # link mass
         self.q = np.zeros([2])  # joint position
+        self.q2 = np.zeros([2])  # joint position
 
     # forward kinematics
-    def FK(self):
+    def FK(self, q):
         p = np.zeros([2])  # endpoint position
         p[0] = self.l[0] * math.cos(self.q[0]) + self.l[1] * math.cos(self.q[1] + self.q[0])
         p[1] = self.l[0] * math.sin(self.q[0]) + self.l[1] * math.sin(self.q[1] + self.q[0])
         return p
 
-    def FK_DH(self):
+    def FK_DH(self, q):
         p = np.zeros([2])  # endpoint position
         # DH parameter
         alpha = np.zeros([2])
@@ -38,8 +41,8 @@ class robot_arm_2dof:
             T_xalpha = np.array(
                 [[1, 0, 0, 0], [0, np.cos(alpha[i]), -np.sin(alpha[i]), 0], [0, np.sin(alpha[i]), np.cos(alpha[i]), 0],
                  [0, 0, 0, 1]])
-            A = A.dot(T_zd.dot(T_ztheta.dot(T_xa).dot( T_xalpha)))
-        p = A.dot(np.array([0,0,0,1]))
+            A = A.dot(T_zd.dot(T_ztheta.dot(T_xa).dot(T_xalpha)))
+        p = A.dot(np.array([0, 0, 0, 1]))
         return p[0:2]
 
     # Jacobian matrix
@@ -66,6 +69,11 @@ class robot_arm_2dof:
         self.q = q
 
 
+def DegtoRad(degree):
+    degree[0] = degree[0] / 180 * np.pi
+    degree[1] = degree[1] / 180 * np.pi
+
+
 '''SIMULATION'''
 
 # SIMULATION PARAMETERS
@@ -81,19 +89,18 @@ l2 = 0.3  # link 2 length
 l = [l1, l2]  # link length
 m = [1.0, 1.0]  # link mass
 ag = 9.81  # acceleration of gravity
-
+q = [0, 90]
+DegtoRad(q)
 # REFERENCE TRAJETORY
 ts = T / dt  # trajectory size
-xt = np.ones(int(ts))
-yt = np.ones(int(ts))
-x = xt
-y = yt
-
-pr = np.array((x / 10 * 1, y / 10*5))  # reference endpoint trajectory
+x = 1
+y = 1
 
 # SIMULATOR
 # initialise robot model class
-model = robot_arm_2dof(l, m, ag)
+model = robot_arm_2dof(l, m)
+model.state(q)
+pr1 = model.FK(q)  # reference endpoint trajectory
 
 # initialise real-time plot with pygame
 pygame.init()  # start pygame
@@ -111,11 +118,6 @@ clock = pygame.time.Clock()  # initialise clock
 FPS = int(1 / dts)  # refresh rate
 
 # initial conditions
-t = 0.0  # time
-q = model.IK(pr[:, 0])  # joint position
-model.state(q)  # update initial state
-p_prev = pr[:, 0]  # previous endpoint position
-i = 0  # loop counter
 state = []  # state vector
 
 # scaling
@@ -131,7 +133,7 @@ while run:
 
 # MAIN LOOP
 run = True
-for i in range(len(x)):
+while run:
     for event in pygame.event.get():  # interrupt function
         if event.type == pygame.QUIT:  # force quit with closing the window
             run = False
@@ -147,8 +149,8 @@ for i in range(len(x)):
 
     # real-time plotting
     window.fill((255, 255, 255))  # clear window
-    pygame.draw.circle(window, (0, 255, 0), (int(window_scale * pr[0, i]) + xc, int(-window_scale * pr[1, i]) + yc),
-                       3)  # draw reference position
+    pygame.draw.circle(window, (0, 255, 0), (int(window_scale * pr1[0]) + xc, int(-window_scale * pr1[1]) + yc),
+                       10)  # draw reference position
     pygame.draw.lines(window, (0, 0, 255), False, [(window_scale * x0 + xc, -window_scale * y0 + yc),
                                                    (window_scale * x1 + xc, -window_scale * y1 + yc),
                                                    (window_scale * x2 + xc, -window_scale * y2 + yc)], 3)  # draw links
@@ -156,32 +158,19 @@ for i in range(len(x)):
                        7)  # draw shoulder / base
     pygame.draw.circle(window, (0, 0, 0), (int(window_scale * x1) + xc, int(-window_scale * y1) + yc), 7)  # draw elbow
     pygame.draw.circle(window, (255, 0, 0), (int(window_scale * x2) + xc, int(-window_scale * y2) + yc),
-                       3)  # draw hand / endpoint
+                       5)  # draw hand / endpoint
 
     text = font.render("FPS = " + str(round(clock.get_fps())), True, (0, 0, 0), (255, 255, 255))
     window.blit(text, textRect)
 
     pygame.display.flip()  # update display
 
-    # DYNAMIC CONTROL
-    p = model.FK_DH()
-    # p = model.FK()
-    print(p)
-    # log states for analysis
-    state.append([t, q[0], q[1], p[0], p[1]])
-
-    # increase loop counter
-    i = i + 1
-
     # try to keep it real time with the desired step time
     clock.tick(FPS)
 
-    if i >= len(x):
-        run = False
-    if run == False:
+    if not run:
         break
 
 pygame.quit()  # stop pygame
 
 '''ANALYSIS'''
-
